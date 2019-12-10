37afc0e0cc5c31dd5e5afbca823a6e3a11239aa2
Joan <johan.rulle@laposte.net>
2019-12-10 19:11:18 +0100

    Fin config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        open: true,
+        hot: true
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ] 
+    }
+}
